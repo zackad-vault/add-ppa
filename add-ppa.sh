3fac770fff0a24c18c954a9cfc22ae027bbc12e7
zackad <zackad.dev@gmail.com>
2017-12-29 21:38:30 +0700

    replace hard coded ubuntu version with dynamic codename version retrieved from `lsb_release -c`. save list into separate file instead of appending to `/etc/apt/sources.list`.

--- add-ppa.sh
+++ add-ppa.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env sh 
 if [ $# -eq 1 ]
 NM=`uname -a && date`
 NAME=`echo $NM | md5sum | cut -f1 -d" "`
+CODENAME=`lsb_release -c | sed 's/\s//g' | cut -d":" -f2`
 then
 	ppa_name=`echo "$1" | cut -d":" -f2 -s`
+	list_name=`echo $ppa_name | sed 's/\//-/g'`
 	if [ -z "$ppa_name" ]
 	then
 		echo "PPA name not found"
@@ -11,7 +13,7 @@ then
 		echo "$0 ppa:user/ppa-name"
 	else
 		echo "$ppa_name"
-		echo "deb http://ppa.launchpad.net/$ppa_name/ubuntu lucid main" >> /etc/apt/sources.list
+		echo "deb http://ppa.launchpad.net/$ppa_name/ubuntu $CODENAME main" >> /etc/apt/sources.list.d/"$list_name.list"
 		apt-get update >> /dev/null 2> /tmp/${NAME}_apt_add_key.txt
 		key=`cat /tmp/${NAME}_apt_add_key.txt | cut -d":" -f6 | cut -d" " -f3`
 		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
