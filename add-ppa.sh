0ed997aed274009fdeae70d9303d512e5b105ea4
zackad <zackad.dev@gmail.com>
2017-12-29 21:55:28 +0700

    print process status

--- add-ppa.sh
+++ add-ppa.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh 
+#!/usr/bin/env sh
 if [ $# -eq 1 ]
 NM=`uname -a && date`
 NAME=`echo $NM | md5sum | cut -f1 -d" "`
@@ -12,12 +12,17 @@ then
 		echo "Utility to add PPA repositories in your debian machine"
 		echo "$0 ppa:user/ppa-name"
 	else
-		echo "$ppa_name"
+		echo "adding ppa:$ppa_name into repository list"
 		echo "deb http://ppa.launchpad.net/$ppa_name/ubuntu $CODENAME main" >> /etc/apt/sources.list.d/"$list_name.list"
+		echo "updating repository list ..."
 		apt-get update >> /dev/null 2> /tmp/${NAME}_apt_add_key.txt
+		echo "adding gpg key"
 		key=`cat /tmp/${NAME}_apt_add_key.txt | cut -d":" -f6 | cut -d" " -f3`
 		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
 		rm -rf /tmp/${NAME}_apt_add_key.txt
+		echo "updating repository list ..."
+		apt-get update
+		echo "done"
 	fi
 else
 	echo "Utility to add PPA repositories in your debian machine"
